3eae9057df946a5e5e7fef13959a678962199e0e
oliwebd <126636431+oliwebd@users.noreply.github.com>
2025-11-18 07:14:41 +0300

    Move notifications to bottom center

---
+++ static/sw.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = 'arabiceg-cache-v1';
+const OFFLINE_ASSETS = [
+  '/',
+  '/manifest.json',
+  '/favicon.svg',
+  '/data/main.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(OFFLINE_ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  const { request } = event;
+  if (request.method !== 'GET') return;
+
+  event.respondWith(
+    caches.match(request).then((cached) => {
+      const fetchPromise = fetch(request)
+        .then((response) => {
+          const copy = response.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(request, copy));
+          return response;
+        })
+        .catch(() => cached);
+
+      return cached || fetchPromise;
+    })
+  );
+});
